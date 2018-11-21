51ad7c3600fc3c61c13fa7656874245d79c1d5f6
Jacky Ko <jackkykokoko@gmail.com>
2018-11-22 00:08:05 +0800

    fix padding bug

--- NiftiDataset.py
+++ NiftiDataset.py
@@ -168,7 +168,7 @@ class Resample(object):
     new_size = tuple(new_size)
 
     resampler = sitk.ResampleImageFilter()
-    resampler.SetInterpolator(2)
+    resampler.SetInterpolator(sitk.sitkBSpline)
     resampler.SetOutputSpacing(new_spacing)
     resampler.SetSize(new_size)
 
@@ -179,6 +179,7 @@ class Resample(object):
     image = resampler.Execute(image)
 
     # resample on segmentation
+    resampler.SetInterpolator(sitk.sitkNearestNeighbor)
     resampler.SetOutputOrigin(label.GetOrigin())
     resampler.SetOutputDirection(label.GetDirection())
     # print("Resampling segmentation...")
@@ -213,19 +214,31 @@ class Padding(object):
     if (size_old[0] >= self.output_size[0]) and (size_old[1] >= self.output_size[1]) and (size_old[2] >= self.output_size[2]):
       return sample
     else:
+      self.output_size = list(self.output_size)
+      if size_old[0] > self.output_size[0]:
+        self.output_size[0] = size_old[0]
+      if size_old[1] > self.output_size[1]:
+        self.output_size[1] = size_old[1]
+      if size_old[2] > self.output_size[2]:
+        self.output_size[2] = size_old[2]
+ 
+      self.output_size = tuple(self.output_size)
+
       resampler = sitk.ResampleImageFilter()
-      resampler.SetInterpolator(2)
       resampler.SetOutputSpacing(image.GetSpacing())
       resampler.SetSize(self.output_size)
 
       # resample on image
+      resampler.SetInterpolator(sitk.sitkBSpline)
       resampler.SetOutputOrigin(image.GetOrigin())
       resampler.SetOutputDirection(image.GetDirection())
       image = resampler.Execute(image)
 
       # resample on label
+      resampler.SetInterpolator(sitk.sitkNearestNeighbor)
       resampler.SetOutputOrigin(label.GetOrigin())
       resampler.SetOutputDirection(label.GetDirection())
+
       label = resampler.Execute(label)
 
       return {'image': image, 'label': label}
@@ -271,6 +284,10 @@ class RandomCrop(object):
 
     roiFilter = sitk.RegionOfInterestImageFilter()
     roiFilter.SetSize([size_new[0],size_new[1],size_new[2]])
+
+    # statFilter = sitk.StatisticsImageFilter()
+    # statFilter.Execute(label)
+    # print(statFilter.GetMaximum(), statFilter.GetSum())
 
     while not contain_label: 
       # get the start crop coordinate in ijk
