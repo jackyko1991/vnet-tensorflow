389d643cfcf19ec773432adae43fecb17007ecad
Jacky <jackkykokoko@gmail.com>
2018-11-23 12:20:56 +0800

    add probability output

--- evaluate.py
+++ evaluate.py
@@ -11,24 +11,27 @@ import math
 import numpy as np
 from tqdm import tqdm
 
+# select gpu devices
+os.environ["CUDA_VISIBLE_DEVICES"] = "0" # e.g. "0,1,2", "0,2" 
+
 # tensorflow app flags
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('data_dir','./data/evaluate',
+tf.app.flags.DEFINE_string('data_dir','./data_dental/evaluate',
     """Directory of evaluation data""")
-# tf.app.flags.DEFINE_string('data_dir','./data/raw_data/nii/batch5',
-#     """Directory of evaluation data""")
-tf.app.flags.DEFINE_string('model_path','./tmp/ckpt/checkpoint-4256.meta',
+tf.app.flags.DEFINE_string('image_filename','image.nii',
+    """Image filename""")
+tf.app.flags.DEFINE_string('model_path','./tmp_dental/ckpt/checkpoint-5665.meta',
     """Path to saved models""")
-tf.app.flags.DEFINE_string('checkpoint_dir','./tmp/ckpt',
+tf.app.flags.DEFINE_string('checkpoint_dir','./tmp_dental/ckpt',
     """Directory of saved checkpoints""")
 tf.app.flags.DEFINE_integer('patch_size',256,
     """Size of a data patch""")
-tf.app.flags.DEFINE_integer('patch_layer',8,
+tf.app.flags.DEFINE_integer('patch_layer',32,
     """Number of layers in data patch""")
-tf.app.flags.DEFINE_integer('stride_inplane', 256,
+tf.app.flags.DEFINE_integer('stride_inplane', 128,
     """Stride size in 2D plane""")
-tf.app.flags.DEFINE_integer('stride_layer',8,
+tf.app.flags.DEFINE_integer('stride_layer',16,
     """Stride size in layer direction""")
 tf.app.flags.DEFINE_integer('batch_size',1,
     """Setting batch size (currently only accept 1)""")
@@ -52,20 +55,19 @@ def evaluate():
     # restore model grpah
     tf.reset_default_graph()
     imported_meta = tf.train.import_meta_graph(FLAGS.model_path)
-    
-    # ops to load data
-    # support multiple image input, but here only use single channel, label file should be a single file with different classes
-    image_filename = 'img.nii.gz'
-    # image_filename = 'image_windowed.nii'
 
     # create transformations to image and labels
     transforms = [
-        NiftiDataset.Normalization(),
-        NiftiDataset.Resample(0.2),
+        # NiftiDataset.Normalization(),
+        NiftiDataset.StatisticalNormalization(2.5),
+        NiftiDataset.Resample(0.25),
         NiftiDataset.Padding((FLAGS.patch_size, FLAGS.patch_size, FLAGS.patch_layer))      
         ]
 
-    with tf.Session() as sess:  
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+
+    with tf.Session(config=config) as sess:  
         print("{}: Start evaluation...".format(datetime.datetime.now()))
 
         imported_meta.restore(sess, tf.train.latest_checkpoint(FLAGS.checkpoint_dir,latest_filename="checkpoint-latest"))
@@ -73,8 +75,11 @@ def evaluate():
         print("{}: Restore checkpoint success".format(datetime.datetime.now()))
         
         for case in os.listdir(FLAGS.data_dir):
+            # ops to load data
+            # support multiple image input, but here only use single channel, label file should be a single file with different classes
+
             # check image data exists
-            image_path = os.path.join(FLAGS.data_dir,case,image_filename)
+            image_path = os.path.join(FLAGS.data_dir,case,FLAGS.image_filename)
 
             if not os.path.exists(image_path):
                 print("{}: Image file not found at {}".format(datetime.datetime.now(),image_path))
@@ -103,6 +108,12 @@ def evaluate():
 
                 image_tfm, label_tfm = sample['image'], sample['label']
 
+                # create empty softmax image in pair with transformed image
+                softmax_tfm = sitk.Image(image_tfm.GetSize(),sitk.sitkFloat32)
+                softmax_tfm.SetOrigin(image_tfm.GetOrigin())
+                softmax_tfm.SetDirection(image.GetDirection())
+                softmax_tfm.SetSpacing(image_tfm.GetSpacing())
+
                 # convert image to numpy array
                 image_np = sitk.GetArrayFromImage(image_tfm)
                 image_np = np.asarray(image_np,np.float32)
@@ -110,9 +121,13 @@ def evaluate():
                 label_np = sitk.GetArrayFromImage(label_tfm)
                 label_np = np.asarray(label_np,np.int32)
 
+                softmax_np = sitk.GetArrayFromImage(softmax_tfm)
+                softmax_np = np.asarray(softmax_np,np.float32)
+
                 # unify numpy and sitk orientation
                 image_np = np.transpose(image_np,(2,1,0))
                 label_np = np.transpose(label_np,(2,1,0))
+                softmax_np = np.transpose(softmax_np,(2,1,0))
 
                 # a weighting matrix will be used for averaging the overlapped region
                 weight_np = np.zeros(label_np.shape)
@@ -159,7 +174,7 @@ def evaluate():
                 # acutal segmentation
                 for i in tqdm(range(len(batches))):
                     batch = batches[i]
-                    pred = sess.run('predicted_label/prediction:0', feed_dict={'images_placeholder:0': batch})
+                    [pred, softmax] = sess.run(['predicted_label/prediction:0','softmax/softmax:0'], feed_dict={'images_placeholder:0': batch})
                     istart = ijk_patch_indices[i][0][0]
                     iend = ijk_patch_indices[i][0][1]
                     jstart = ijk_patch_indices[i][0][2]
@@ -167,13 +182,17 @@ def evaluate():
                     kstart = ijk_patch_indices[i][0][4]
                     kend = ijk_patch_indices[i][0][5]
                     label_np[istart:iend,jstart:jend,kstart:kend] += pred[0,:,:,:]
+                    softmax_np[istart:iend,jstart:jend,kstart:kend] += softmax[0,:,:,:,1]
                     weight_np[istart:iend,jstart:jend,kstart:kend] += 1.0
 
+                print("{}: Evaluation complete".format(datetime.datetime.now()))
                 # eliminate overlapping region using the weighted value
                 label_np = np.rint(np.float32(label_np)/np.float32(weight_np) + 0.01)
+                softmax_np = softmax_np/np.float32(weight_np)
 
                 # convert back to sitk space
                 label_np = np.transpose(label_np,(2,1,0))
+                softmax_np = np.transpose(softmax_np,(2,1,0))
 
                 # convert label numpy back to sitk image
                 label_tfm = sitk.GetImageFromArray(label_np)
@@ -181,19 +200,35 @@ def evaluate():
                 label_tfm.SetDirection(image.GetDirection())
                 label_tfm.SetSpacing(image_tfm.GetSpacing())
 
+                softmax_tfm = sitk.GetImageFromArray(softmax_np)
+                softmax_tfm.SetOrigin(image_tfm.GetOrigin())
+                softmax_tfm.SetDirection(image.GetDirection())
+                softmax_tfm.SetSpacing(image_tfm.GetSpacing())
+
                 # resample the label back to original space
                 resampler = sitk.ResampleImageFilter()
+                # save segmented label
+                writer = sitk.ImageFileWriter()
+
                 resampler.SetInterpolator(1)
                 resampler.SetOutputSpacing(image.GetSpacing())
                 resampler.SetSize(image.GetSize())
                 resampler.SetOutputOrigin(image.GetOrigin())
                 resampler.SetOutputDirection(image.GetDirection())
+                
+                print("{}: Resampling label back to original image space...".format(datetime.datetime.now()))
                 label = resampler.Execute(label_tfm)
-
-                # save segmented label
-                writer = sitk.ImageFileWriter()
-                writer.SetFileName(os.path.join(FLAGS.data_dir,case,'label_vnet.nii.gz'))
+                label_path = os.path.join(FLAGS.data_dir,case,'label_vnet.nii.gz')
+                writer.SetFileName(label_path)
                 writer.Execute(label)
+                print("{}: Save evaluate label at {} success".format(datetime.datetime.now(),label_path))
+
+                print("{}: Resampling probability map back to original image space...".format(datetime.datetime.now()))
+                prob = resampler.Execute(softmax_tfm)
+                prob_path = os.path.join(FLAGS.data_dir,case,'probability_vnet.nii.gz')
+                writer.SetFileName(prob_path)
+                writer.Execute(prob)
+                print("{}: Save evaluate probability map at {} success".format(datetime.datetime.now(),prob_path))
 
 def main(argv=None):
     evaluate()
