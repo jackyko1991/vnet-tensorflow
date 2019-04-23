84d8757e48caaa301463401cdc2593013916c08a
Jacky <jackkykokoko@gmail.com>
2019-04-23 13:28:15 +0800

    add dice and jaccard

--- batch_evaluate.py
+++ batch_evaluate.py
@@ -9,6 +9,21 @@ def dist(a,b):
 		x += (a[i]-b[i])**2
 	return math.sqrt(x)
 
+def overlapMeasure(imageA, imageB, method="dice"):
+	if not (method == "dice" or method == "jaccard"):
+		print("invalid method")
+		return 0
+
+	overlapFilter = sitk.LabelOverlapMeasuresImageFilter()
+	overlapFilter.Execute(imageA, imageB)
+
+	if method == "dice":
+		return overlapFilter.GetDiceCoefficient()
+	elif method == "jaccard":
+		return overlapFilter.GetJaccardCoefficient()
+	else:
+		return 0
+
 def Accuracy(gtName,outputName, tolerence=3):
 	reader = sitk.ImageFileReader()
 	reader.SetFileName(gtName)
@@ -19,9 +34,16 @@ def Accuracy(gtName,outputName, tolerence=3):
 
 	# cast label to int
 	castFilter = sitk.CastImageFilter()
-	castFilter.SetOutputPixelType(sitk.sitkInt16)
+	castFilter.SetOutputPixelType(sitk.sitkUInt16)
 	groundTruth = castFilter.Execute(groundTruth)
 	output = castFilter.Execute(output)
+	output.SetOrigin(groundTruth.GetOrigin())
+	output.SetDirection(groundTruth.GetDirection())
+	output.SetSpacing(groundTruth.GetSpacing())
+
+	# dice and jaccard
+	dice = overlapMeasure(groundTruth, output,"dice")
+	jaccard = overlapMeasure(groundTruth, output,"jaccard")
 
 	# locate ground truth location
 	ccFilter = sitk.ConnectedComponentImageFilter()
@@ -72,7 +94,7 @@ def Accuracy(gtName,outputName, tolerence=3):
 	print("FN:",FN)
 	print("Sensitivity:", sensitivity)
 	print("IoU:", iouScore)
-	return TP, FP, FN
+	return TP, FP, FN, dice, jaccard
 
 def main():
 	ckpt_dir = "./tmp/ckpt"
@@ -90,7 +112,7 @@ def main():
 		checkpoint_path = os.path.join(ckpt_dir,ckpt.split(".")[0])
 		checkpoint_num = int(ckpt.split(".")[0].split("-")[1])
 
-		if checkpoint_num < 30000:
+		if checkpoint_num < 35000:
 			continue
 		command = "python evaluate.py " + \
 			"--model_path " + model_path +  " " +\
@@ -102,21 +124,25 @@ def main():
 		TP = 0
 		FP = 0
 		FN = 0
+		DICE = 0
+		Jaccard = 0
 
 		if os.path.exists(os.path.join(output_csv_dir,str(checkpoint_num)+".csv")):
 			os.remove(os.path.join(output_csv_dir,str(checkpoint_num)+".csv"))
 		csvfile = open(os.path.join(output_csv_dir,str(checkpoint_num)+".csv"), 'w')
 		filewriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		filewriter.writerow(['Case', 'TP', 'FP', 'FN', 'Sensitivity', 'IoU'])
+		filewriter.writerow(['Case', 'TP', 'FP', 'FN', 'Sensitivity', 'IoU', 'DICE', 'Jaccard'])
 
 		for case in os.listdir(dataDir):
 			if not os.path.exists(os.path.join(dataDir,case,"label_vnet.nii.gz")):
 				continue
 			print("case:",case)
-			_TP, _FP, _FN = Accuracy(os.path.join(dataDir,case,"label_crop.nii.gz"),os.path.join(dataDir,case,"label_vnet.nii.gz"))
+			_TP, _FP, _FN, _DICE, _Jaccard = Accuracy(os.path.join(dataDir,case,"label_crop.nii.gz"),os.path.join(dataDir,case,"label_vnet.nii.gz"))
 			TP += _TP
 			FP += _FP
 			FN += _FN
+			DICE += _DICE
+			Jaccard += _Jaccard
 
 			if (_TP+_FN) == 0:
 				_sensitivity = "nan"
@@ -126,9 +152,8 @@ def main():
 				_iou = "nan"
 			else:
 				_iou = _TP/(_TP+_FP+_FN)
+			filewriter.writerow([case, _TP, _FP, _FN, _sensitivity, _iou, _DICE, _Jaccard])
 
-			filewriter.writerow([case, _TP, _FP, _FN, _sensitivity, _iou])
-		csvfile.close()
 
 		print("Evaluation result of checkpoint {}:".format(ckpt))
 		if (TP+FN) == 0:
@@ -142,7 +167,9 @@ def main():
 
 		print("Average Sensitivity:", avg_sensitivity)
 		print("Average IoU:", avg_iou)
-		exit()
+
+		filewriter.writerow(["average", TP, FP, FN, avg_sensitivity, avg_iou, DICE/len(os.listdir(dataDir)), Jaccard/len(os.listdir(dataDir))])
+		csvfile.close()
 
 if __name__=="__main__":
 	main()
